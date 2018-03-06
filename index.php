b92980937951ea3cc30172434a178ee4f46a5dcf
borikenmedia <37109363+borikenmedia@users.noreply.github.com>
2018-03-06 09:05:18 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+date_default_timezone_set("America/Puerto_Rico");
+header("Content-type: text/html; Charset: utf-8; Pragma: no-cache;");
+/* Micro CMS v2 Release Under GPL3 By dyewilliam */
+include("config.php");
+
+echo("Under Construction");
+
+?>
