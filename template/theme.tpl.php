135c3dd5d2371fabde0f9a68e8ee29124386c9e3
borikenmedia <37109363+borikenmedia@users.noreply.github.com>
2018-03-06 09:00:16 -0400

    Create theme.tpl.php

---
+++ template/theme.tpl.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html Public>
+<html lang="en-us">
+<head>
+<title>{web_title}</title>
+<meta name="description" content="Demo website template" />
+<meta name="keywords" content="Web, template, php" />
+<meta name="author" content="dyewilliam" />
+<meta charset="utf-8" />
+<meta name="view-port" content="width: device-width, initial-scale: 1" />
+<style type="text/css">
+<!--
+
+//-->
+</style>
+</head>
+<body>
+
+<h3>{under_construction}</h3>
+
+</body>
+</html>
