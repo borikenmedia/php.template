52607042ee63dafe88946c03cb013f6ebb174e7d
borikenmedia <37109363+borikenmedia@users.noreply.github.com>
2018-09-01 22:55:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ header("Content-type: text/html; Charset: utf-8; Pragma: no-cache;");
 /* Micro CMS v2 Release Under GPL3 By dyewilliam */
 include("config.php");
 
-$tpl = new template();
+$tpl = new template($conf["tpl_file_path"]);
 
-$tpl->parse_tpl($tags)->output();
+$tpl->parse_tpl($conf["tpl_file_tags"])->display_tpl();
 
 ?>
