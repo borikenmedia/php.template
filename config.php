4a6eba00f30b3081b9aab15eb441e8f4b8e3c890
borikenmedia <37109363+borikenmedia@users.noreply.github.com>
2018-09-01 22:53:29 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,18 @@
 <?php
 /* Micro CMS v2 Release Under GPL3 By dyewilliam (DOCS) */
 
-$path = array();
-$path["tpl_file"] = "./template/template.php";
+$conf = array();
+// Set a ($)conf variable as an array (multidimendional_array)
+
+$conf["tpl_file_path"] = "./template/template.php";
 // Set our template file path
+
+$conf["tpl_file_tags"] = array();
+// Set our template file tags variable as an array
+$conf["tpl_file_tags"]["title"] = "Boriken Media LLP 2018";
+$conf["tpl_file_tags"]["menu"] = "template/menu.tpl.php";
+$conf["tpl_file_tags"]["body"] = "hello world";
+$conf["tpl_file_tags"]["footer"] = "Copyright &copy; 2018 - Some rights reserved";
+// Set values for all our template file tags variables
 
 ?>
